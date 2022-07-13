574b63b42dffa9481b42fd65509c27c362c3a2a7
DarviL <davidlosantos89@gmail.com>
2022-07-13 11:14:16 +0200

    add: testing project and more progress on modal system

---
+++ src/Zebra/Services/ModalService.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Components;
+using Zebra.Components;
+
+namespace Zebra.Services;
+
+public class ModalContent : EventArgs
+{
+	public string? Title { get; set; }
+	public RenderFragment Content { get; set; } = default!;
+}
+
+public class ModalService
+{
+	public EventHandler<EventArgs>? OnClose { get; set; }
+	public EventHandler<ModalContent>? OnOpen { get; set; }
+
+	public void Close()
+	{
+		OnClose?.Invoke(this, EventArgs.Empty);
+	}
+
+	public void Open<T>(ModalTemplate<T> content)
+	{
+		// OnOpen?.Invoke(this, content);
+	}
+}
